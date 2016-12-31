65ce20064f4c7b3ef440bb9c059508c9441bd655
Michael <mike.karpinski99@gmail.com>
2016-12-31 23:14:26 +0000

    added validation

--- server.js
+++ server.js
@@ -6,16 +6,15 @@ var url = process.env.DB_URI;
 app.get('/new/*', (req, res) => {
   mongo.connect(url, function (err, db) {
     if (err) throw err;
-    var n = Math.floor((Math.random() * 10000) + 1);
-    while(db.collection('data').find({'newUrl': n}).count > 0){
-      n = Math.floor((Math.random() * 10000) + 1);
+    var num = Math.floor((Math.random() * 10000) + 1);
+    while(db.collection('data').find({'newUrl': num}).count > 0){
+      num = Math.floor((Math.random() * 10000) + 1);
     }
-    db.collection('data').insert({'old': req.params[0], 'newUrl': n}, (err, doc) => {
+    db.collection('data').insert({'old': req.params[0], 'newUrl': num}, (err, doc) => {
       if(err) throw err;
-      var id = doc.insertedIds[0];
       var n = 'https://' + req.headers.host + '/' + doc.ops[0].newUrl;
       var o = doc.ops[0].old;
-      var obj = {'original_url': o, 'new_url': n}
+      var obj = {'original_url': o, 'new_url': n};
       res.end(JSON.stringify(obj));
     });
   
@@ -25,25 +24,39 @@ app.get('/new/*', (req, res) => {
 
 app.get('/:id', (req, res) => {
   var u;
-  var param = req.params.id;
-  mongo.connect(url, function (err, db) {
-    if(err) throw err;
-    var data = db.collection('data');
-    data.find({'newUrl' : Number(param)}, {
-        old: 1
-    }).toArray(function(err, docs){
-        if(err) throw err;
-        u = docs[0].old;
-        console.log(u);
-        if(u.substring(0, 4) !== 'http'){
-          res.redirect('http://' + u);
-        }
-        else{
-          res.redirect(u);
-        }
+  var param = Number(req.params.id);
+  if(param.toString() == req.params.id){
+    mongo.connect(url, function (err, db) {
+      if(err) throw err;
+      var data = db.collection('data');
+      if(data.find({'newUrl' : param}).count == 0){
+        res.end('That URL is not valid!');
         db.close();
+      }
+      else{
+        data.find({'newUrl' : param}, {
+            old: 1
+        })
+        .toArray(function(err, docs){
+          if(err) throw err;
+          console.log(docs);
+          u = docs[0].old;
+          console.log(u);
+          if(u.substring(0, 4) !== 'http'){
+            res.redirect('http://' + u);
+          }
+          else{
+            res.redirect(u);
+          }
+          db.close();
+        });
+      }
     });
-  });
+  }
+  
+  else{
+    res.end('Please enter a valid ID.');
+  }
 });
 
 app.listen(process.env.PORT);
